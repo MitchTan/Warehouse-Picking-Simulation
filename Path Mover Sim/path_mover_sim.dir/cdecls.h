ed542929d85d9690c2bcc015aea8d932376ebec0
sakib92farooque <sakibbfru92@gmail.com>
2015-10-04 20:59:14 +0800

    Path Mover System
    Incomplete attempt at PM

---
+++ Path Mover Sim/path_mover_sim.dir/cdecls.h
@@ -0,0 +1,207 @@
+// cdecls.h
+// AutoMod 12.6.1 Generated File
+// Build: 12.6.1.12
+// Model name:	path_mover_sim
+// Model path:	C:\Users\Sakib92\OneDrive\Documents\Sem 5\IE3110\Project\Path Mover Sim\path_mover_sim.dir\
+// Generated:	Sun Oct 04 17:09:02 2015
+// Applied/AutoMod Licensee Confidential
+// NO DISTRIBUTION OR REPRODUCTION RIGHTS GRANTED!
+// Copyright (c) 1988-2015 Applied Materials All rights reserved.
+//
+// All Rights Reserved.  Reproduction or transmission in whole or
+// in part, in any form or by any means, electronic, mechanical or
+// otherwise, is prohibited without the prior written consent of
+// copyright owner.
+//
+// Licensed Material - Property of Applied Materials, Inc.
+//
+// Applied Materials, Inc.
+// 3050 Bowers Drive
+// P.O. Box 58039
+// Santa Clara, CA 95054-3299
+// U.S.A.
+//
+
+
+#include "decls.h"
+
+#undef am2_S1_0_6
+#define am2_S1_0_6 am_model.am_Path_Mover.am_S1_0_6
+#undef am2_S1_0_4
+#define am2_S1_0_4 am_model.am_Path_Mover.am_S1_0_4
+#undef am2_DefSegment
+#define am2_DefSegment am_model.am_Path_Mover.am_DefSegment
+#undef am2_S1_0_3
+#define am2_S1_0_3 am_model.am_Path_Mover.am_S1_0_3
+#undef am2_S1_0_2
+#define am2_S1_0_2 am_model.am_Path_Mover.am_S1_0_2
+#undef am2_S1_0_1
+#define am2_S1_0_1 am_model.am_Path_Mover.am_S1_0_1
+#undef am2_Picker
+#define am2_Picker am_model.am_Path_Mover.am_Picker
+#undef am2_S1_0_10
+#define am2_S1_0_10 am_model.am_Path_Mover.am_S1_0_10
+#undef am2_S1_2_20
+#define am2_S1_2_20 am_model.am_Path_Mover.am_S1_2_20
+#undef am2_DefVehicle
+#define am2_DefVehicle am_model.am_Path_Mover.am_DefVehicle
+#undef am2_parentsys
+#define am2_parentsys am_model.am_Path_Mover.am_parentsys
+#undef GetSocketNonBlocking
+#undef XLClear
+#undef OPCFlushWrite
+#undef OPCInit
+#undef SetSocketMessages
+#undef Fsysdisplay
+#undef OPCWriteIntegerWait
+#undef Fsetpathtime
+#undef SendSocketString
+#undef GetSocketTimeOut
+#undef FsetStartDate
+#undef MMSyncSendMessageNumReals
+#undef OPCListConnections
+#undef MMSyncSendMessageNumIntegers
+#undef XLClearAreaR1C1
+#undef MMSyncSendMessagePrint
+#undef AcceptSocketService
+#undef XLClearR1C1
+#undef SetSocketNonBlocking
+#undef DBGetErrMsg
+#undef FtranQueue
+#undef Fquetoggle
+#undef OPCWriteStringWait
+#undef XLGetAreaA1
+#undef XLSetA1
+#undef SetSocketTimeOut
+#undef FtranResource
+#undef DBOpenSession
+#undef MMSyncReadMessagePrint
+#undef FscaleQueue
+#undef MMSyncReadMessageNumIntegers
+#undef MMSyncReadMessageNumReals
+#undef MMSyncSendMessageString
+#undef MilliSleep
+#undef SetAcceptTimeOut
+#undef ValidSocket
+#undef MMSyncSendMessageClear
+#undef FattachRes
+#undef AdjacentLocs
+#undef OPCReadInteger
+#undef DBMoveLast
+#undef MMSyncReadMessageString
+#undef DBMoveFirst
+#undef LocPathName
+#undef Fchangename
+#undef XLGetAreaR1C1
+#undef GetAnimate
+#undef FgetModelDate
+#undef XLSetAreaA1
+#undef am2_OPCTimestamp
+#define am2_OPCTimestamp am_model.am_OPCTimestamp
+#undef FireUserEvent
+#undef ConnectSocketPort
+#undef OPCDisconnect
+#undef GetSocketNum
+#undef ConnectSocketService
+#undef FcolorElement
+#undef OPCWriteString
+#undef XLGetA1
+#undef CurPath
+#undef MMSyncSendMessage
+#undef DBGetColumnName
+#undef Fresvis
+#undef DBMovePrev
+#undef GetSocketHost
+#undef FattachRobot
+#undef MMSyncReadMessageStringLength
+#undef OPCAddGroup
+#undef Fvehdisplay
+#undef OPCWriteInteger
+#undef FrotResource
+#undef FcopyQueuePict
+#undef FgetStartDate
+#undef XLSetAreaR1C1
+#undef ReadSocketString
+#undef FrotQueue
+#undef LocPath
+#undef OPCReadReal
+#undef am2_modelsys
+#define am2_modelsys am_model.am_modelsys
+#undef OPCSubscribe
+#undef XLClearArea
+#undef MachineName
+#undef OPCConnectServer
+#undef MMSyncSendMessageReal
+#undef DBOpenSourceUDL
+#undef OPCWriteRealWait
+#undef MMSyncSendMessageInteger
+#undef XLClearColumnC1
+#undef DistLoc2Loc
+#undef XLClearAreaA1
+#undef OPCReadString
+#undef DBOpenSource
+#undef MMSyncReadMessageReal
+#undef FattachLabel
+#undef XLSetR1C1
+#undef XLClearA1
+#undef DBGetRowData
+#undef XLGet
+#undef OPCListItems
+#undef DBGetColumnCount
+#undef am2_Path_Mover
+#define am2_Path_Mover am_model.am_Path_Mover.$sys
+#undef MMSyncReadMessageInteger
+#undef XLClearRow
+#undef ClosestVehDests
+#undef AcceptSocketPort
+#undef FgetNavDist
+#undef MMSyncReadMessageType
+#undef XLClearWorkbook
+#undef am2_stream0
+#define am2_stream0 am_model.am_stream0
+#undef SetNumAcceptsPort
+#undef SetNumAcceptsService
+#undef SetAcceptBlocking
+#undef XLGetArea
+#undef DBExecuteCmd
+#undef FcopyLoadTypePict
+#undef MMSyncSendMessageType
+#undef SetSyncRate
+#undef OPCAddItem
+#undef XLClearSheet
+#undef Fquevis
+#undef SetConnectTimeOut
+#undef MMSyncSendMessageNumDatas
+#undef FauthToolSimModule
+#undef CloseSocket
+#undef XLSet
+#undef XLGetR1C1
+#undef DBCloseSession
+#undef OPCRefreshSubscription
+#undef OPCWriteReal
+#undef MMSyncSendMessageModel
+#undef SetConnectBlocking
+#undef DBMoveNext
+#undef GetActiveXRunning
+#undef XLClose
+#undef XLClearColumnA1
+#undef MMSyncReadMessageTime
+#undef FscaleResource
+#undef GetSocketName
+#undef XLSetArea
+#undef ClosestVehicles
+#undef DBCloseSource
+#undef Fsaa_on
+#undef MMSyncSendMessageNumStrings
+#undef Fdelpic
+#undef DistVeh2Loc
+#undef Fgetsysdate
+#undef FtoggleSolid
+#undef Fsystem_seconds
+#undef FattachQueue
+#undef am2_OPCQuality
+#define am2_OPCQuality am_model.am_OPCQuality
+#undef MMSyncReadMessageNumStrings
+#undef Fmodnavi
+#undef Ftogglepict
+#undef SetDisplayStep
